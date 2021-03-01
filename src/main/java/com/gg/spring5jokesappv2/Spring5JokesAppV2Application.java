08943781b7dd077983c8008b2ea24a2402c3c189
MTocchettoP <marcostp1995@hotmail.com>
2021-03-01 13:08:23 -0700

    Added xml based config

---
+++ src/main/java/com/gg/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,19 @@
+package com.gg.spring5jokesappv2;
+
+import com.gg.spring5jokesappv2.services.JokeService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
+public class Spring5JokesAppV2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Spring5JokesAppV2Application.class, args);
+
+    }
+
+}
